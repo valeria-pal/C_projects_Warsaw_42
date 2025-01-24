fb248a6a9a776ec83b4b44016c9097de3cc874f6
Valeryia Paliashchuk <vpaliash@c1r7s3.42warsaw.pl>
2025-01-24 16:46:04 +0100

    Added changes to printf

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: vpaliash <vpaliash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:37:06 by vpaliash          #+#    #+#             */
-/*   Updated: 2025/01/22 20:37:44 by vpaliash         ###   ########.fr       */
+/*   Updated: 2025/01/24 15:41:53 by vpaliash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,40 @@ void	ft_putchar_fd(char c, int fd)
 	write(fd, &c, 1);
 }
 
+static void	print_nbr(int n, int fd)
+{
+	char	digit;
+
+	digit = "0123456789"[n];
+	write(fd, &digit, 1);
+}
+
+static void	ft_putnbr_fd(int n, int fd)
+{
+	long	nbr;
+
+	nbr = n;
+	if (nbr < 0)
+	{
+		write(fd, "-", 1);
+		nbr = -nbr;
+	}
+	if (nbr >= 10)
+		ft_putnbr_fd(nbr / 10, fd);
+	print_nbr(nbr % 10, fd);
+}
+
 static void ft_handle_specifiers(char specifier, int * counter, va_list args)
 {
 	if (specifier == 's')
 	ft_putstr_fd(va_arg(args,char *), 1);
-	else if (specifier == 'c')
+	if (specifier == 'c')
 	ft_putchar_fd(va_arg(args,char *), 1);
-	
+	if (specifier == 'd')
+	ft_putnbr_fd(va_arg(args,int),1);
+	if (specifier == 'f')
+	ft_putnbr_fd(va_arg(args,int),1);
+
 	
 }
 
@@ -55,7 +82,7 @@ int ft_printf(const char * format, ...)
 		if (*format == '%')
 		{
 			format++;
-		ft_handle_specifiers(*format, &counter, &args);
+		ft_handle_specifiers(*format, &counter, args);
 		format++;
 	
 		}
@@ -68,9 +95,9 @@ int ft_printf(const char * format, ...)
 	int main ()
 	{	
 		printf("ORIGINAL PRINTF \n");
-		printf("My name is %s , I have %d years old ", "Alice" , 15);
+		printf("My name is %s , I have %d years old and %f", "Alice" ,-14,  - 3.5);
 		printf("\nMY PRINTF \n");
-		ft_printf("My name is %s!",  "Alice");
+		ft_printf("My name is %s , first letter is %c, I have %d years old and %f",  "Alice", 'A', -23,1.5);
 		return 0;
 	}
 
