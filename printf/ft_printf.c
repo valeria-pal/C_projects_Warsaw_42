d2e740c3ffa67b7c2da719b50e381b27133a8152
Valeryia Paliashchuk <vpaliash@c1r7s3.42warsaw.pl>
2025-02-07 15:31:29 +0100

    Changes added

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,13 +6,15 @@
 /*   By: vpaliash <vpaliash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:37:06 by vpaliash          #+#    #+#             */
-/*   Updated: 2025/02/04 13:26:03 by vpaliash         ###   ########.fr       */
+/*   Updated: 2025/02/07 15:12:10 by vpaliash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdint.h>
+
 
 
 static void	ft_putstr_fd(char *s, int fd)
@@ -37,7 +39,7 @@ static void	print_nbr(int n, int fd)
 	write(fd, &digit, 1);
 }
 
-static void	ft_putnbr_fd(int n, int fd)
+static void	ft_putnbr_fd(int n, int fd)//mistake for unsigned
 {
 	long	nbr;
 
@@ -50,6 +52,34 @@ static void	ft_putnbr_fd(int n, int fd)
 	if (nbr >= 10)
 		ft_putnbr_fd(nbr / 10, fd);
 	print_nbr(nbr % 10, fd);
+	
+}
+static void print_decimal(int n, int fd , char specifier)
+{
+	char lowercase;
+	char uppercase;
+	if (specifier == 'x')
+	{
+	lowercase = "0123456789abcdef"[n];
+	write(1,&lowercase,1);
+	}
+	if (specifier == 'X')
+	{
+	uppercase = "0123456789ABCDEF"[n];
+	write(1,&uppercase,1);
+	}
+	if (specifier == 'p')
+	write(1,&lowercase,1);
+}
+
+static void ft_convert_to_hexadecimal(unsigned int decimal,int fd, char specifier)
+{	
+	unsigned long decimal_nbr;
+	decimal_nbr =  decimal;
+
+	if (decimal_nbr >= 16)
+	ft_convert_to_hexadecimal(decimal_nbr / 16, fd,specifier);
+	print_decimal(decimal_nbr % 16, fd, specifier);
 }
 
 static void ft_handle_specifiers(char specifier, int * counter, va_list args)
@@ -57,9 +87,21 @@ static void ft_handle_specifiers(char specifier, int * counter, va_list args)
 	if (specifier == 's')
 	ft_putstr_fd(va_arg(args,char *), 1);
 	if (specifier == 'c')
-	ft_putchar_fd(va_arg(args,int ), 1);
+	ft_putchar_fd(va_arg(args, int), 1);
 	if (specifier == 'd'  || specifier == 'i')
-	ft_putnbr_fd(va_arg(args,int),1);	
+	ft_putnbr_fd(va_arg(args,int),1);
+	if(specifier == 'u')
+	ft_putnbr_fd(va_arg(args,unsigned int),1);
+	if (specifier == '%')
+	write(1, "%",1);
+	if(specifier == 'x' || specifier == 'X')
+	ft_convert_to_hexadecimal(va_arg(args, int),1,specifier);
+	if(specifier == 'p')
+	{
+		write(1, "0x",2);
+		ft_convert_to_hexadecimal(va_arg(args, uintptr_t),1,specifier);
+		
+	}
 }
 
 int ft_printf(const char * format, ...)
@@ -91,9 +133,11 @@ int ft_printf(const char * format, ...)
 
 	int main ()
 	{	
+		int num = 42;  
+		int *ptr = &num;
 		printf("ORIGINAL PRINTF \n");
-		printf("My name is %s , first letter is %c, I have %d years old and %i sisters",  "Alice", 'A', -1, 3);
+		printf("My name is %s and %%, first letter is %c, I have %d years old and %i sisters and some %u, now hexadecimal number is %x and %X, pointer is %p","Alice", 'A', -1,  12,-11,122, 419, ptr);
 		printf("\nMY PRINTF \n");
-		ft_printf("My name is %s , first letter is %c, I have %d years old and %i sisters",  "Alice", 'A', -1,3);
+		ft_printf("My name is %s and %%, first letter is %c, I have %d years old and %i sisters and some %u, now hexadecimal number is %x and %X, pointer is %p","Alice", 'A', -1,  12,-11,122, 419, ptr);
 		return 0;
 	}
