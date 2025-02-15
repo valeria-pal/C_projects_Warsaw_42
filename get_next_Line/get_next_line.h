7859b9360707367dcbd96a4c9e38142975d0aac2
Valeryia Paliashchuk <vpaliash@c1r6s3.42warsaw.pl>
2025-02-15 13:51:45 +0100

    added structure of project

---
+++ get_next_Line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpaliash <vpaliash@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/14 15:18:17 by vpaliash          #+#    #+#             */
+/*   Updated: 2025/02/14 16:21:13 by vpaliash         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#define BUFFER_SIZE = n
+
+#include <stdlib.h>
+
+char *get_next_line(int fd);
+
+#endif
