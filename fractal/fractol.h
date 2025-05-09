75a365979ab46a5793bb0a7ed79624e19a909286
valeria-pal <vallery7543@gmail.com>
2025-05-09 15:23:20 +0200

    first mlx functions added

--- fractal/fractol.h
+++ fractal/fractol.h
@@ -1,28 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   fractol.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpaliash <vpaliash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/09 13:45:46 by vpaliash          #+#    #+#             */
-/*   Updated: 2025/05/09 15:22:51 by vpaliash         ###   ########.fr       */
+/*   Created: 2025/05/09 13:15:38 by vpaliash          #+#    #+#             */
+/*   Updated: 2025/05/09 13:49:24 by vpaliash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fractol.h>
-
-int main()
-{
-	void *mlx_connection_to_server;
-	void *mlx_window;
-	
-	mlx_connection_to_server = mlx_init();
-	if(!mlx_connection_to_server)
-	return (1);
-	
-	//mlx_window = mlx_new_window
-	mlx_destroy_display(mlx_connection_to_server);
-
-	return(0);
-}
+# include "libft/libft.h"
+#include "minilibx/mlx.h"
